7298c54a776612bccec8f13a76b1c5f3ecb05f0a
Aine-dickson <ainedixon01@gmail.com>
2025-02-01 09:42:46 +0300

    Basic app flow

---
+++ app/awh/entity/src/prelude.rs
@@ -0,0 +1,4 @@
+pub use super::user::Entity as User;
+pub use super::verification::Entity as Verification;
+pub use super::user::ActiveModel as ActiveUser;
+pub use super::verification::ActiveModel as ActiveVerification;
